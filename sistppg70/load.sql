c9260e21b5194a480a4ba4ef502785b4564d4e4c
chitty.carlosj@gmail.com <chitty.carlosj@gmail.com@08de626e-1138-83a5-deb6-95a31232f83a>
2011-03-23 19:38:42 +0000

    LOAD.SQL

---
+++ sistppg70/load.sql
@@ -0,0 +1,32 @@
+insert into usuario values (1, '07-41896', 'estudiante');
+insert into usuario values (2, 'zchiquin', 'profesor');
+
+insert into carrera values (1, 'Ingeniería Eléctrica', 'coord-ee@usb.ve');
+insert into carrera values (2, 'Ingeniería de Computación', 'coord-comp@usb.ve');
+insert into carrera values (3, 'Ingeniería Mecánica', 'coord-mec@usb.ve');
+insert into carrera values (4, 'Ingeniería Eléctronica', 'coord-electronia@usb.ve');
+
+insert into departamento values (1, 'Computación y Tecnología de la Información');
+insert into departamento values (2, 'Procesos y Sistemas');
+insert into departamento values (3, 'Física');
+insert into departamento values (4, 'Química');
+insert into departamento values (5, 'Computo Científico y Estadística');
+insert into departamento values (6, 'Electrónica y Circuitos');
+
+insert into area values (1, 'Bases de Datos');
+insert into area values (2, 'Lenguajes y Algoritmos');
+insert into area values (3, 'Inteligencia Artificial');
+insert into area values (4, 'Sistemas Paralelos y Distribuidos');
+insert into area values (5, 'Gráficas, Interfaces y Multimedia');
+insert into area values (6, 'Procesos Químicos');
+insert into area values (7, 'Sistemas de Información');
+insert into area values (8, 'Sistemas de Control');
+
+insert into departamentoarea values (1, 1, 1);
+insert into departamentoarea values (2, 2, 1);
+insert into departamentoarea values (3, 3, 1);
+insert into departamentoarea values (4, 4, 1);
+insert into departamentoarea values (5, 5, 1);
+insert into departamentoarea values (6, 6, 2);
+insert into departamentoarea values (7, 7, 2);
+insert into departamentoarea values (8, 8, 2);
